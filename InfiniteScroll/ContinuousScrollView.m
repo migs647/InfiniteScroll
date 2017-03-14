54d6062651c6b23e2199a3713b6e350cde8639c1
Cody Garvin <cgarvin@walmartlabs.com>
2017-03-13 22:00:17 -0700

    Added first pass at infinite scrolling

--- InfiniteScroll/ContinuousScrollView.m
+++ InfiniteScroll/ContinuousScrollView.m
@@ -12,23 +12,49 @@ NSInteger kContinuousScrollViewLabelTopMargin = 32;
 NSInteger kContinuousScrollViewLabelSideMargin = 16;
 
 @interface ContinuousScrollView () <UIScrollViewDelegate>
-@property (nonatomic, strong) NSMutableArray *labelsLaidout;
+@property (nonatomic, strong) NSMutableArray *labelContainersLaidout;
 @property (nonatomic, strong) UIView *currentContainerView;
+@property (nonatomic, strong) NSArray *verticalLabelConstraints;
 @end
 
 @implementation ContinuousScrollView
 
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self)
+    {
+        // Hide the scroll indicator so we can appear seemless
+        [self setShowsVerticalScrollIndicator:NO];
+    }
+    
+    return self;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self)
+    {
+        // Hide the scroll indicator so we can appear seemless
+        [self setShowsVerticalScrollIndicator:NO];
+    }
+    
+    return self;
+}
+
 - (void)reload {
     
     // Reset our world
-    self.labelsLaidout = nil;
+    self.labelContainersLaidout = nil;
     self.currentContainerView = nil;
     
-    // Get the container view ready
+    // Get the container view ready -- This holds the sets of containers for labels
+    // that we will move around
     [self buildContainerView];
     
-    // Build a vertical constraint string for the items
-    [self addLabels];
+    // Tile out the labels -- should only need 3 sets total
+    [self tileLabels];
     
     // Now layout all of our subviews
     [self setNeedsLayout];
@@ -61,23 +87,62 @@ NSInteger kContinuousScrollViewLabelSideMargin = 16;
     [self addConstraint:[NSLayoutConstraint constraintWithItem:_currentContainerView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0]];
 }
 
-- (void)addLabels
+- (void)tileLabels
 {
+    // Grab three sets and build their constraints
+    UIView *labelSet1 = [self buildLabelSet];
+    [self.currentContainerView addSubview:labelSet1];
+     [NSLayoutConstraint activateConstraints:
+      [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[labelSet1]|"
+                                              options:0 metrics:0
+                                                views:NSDictionaryOfVariableBindings(labelSet1)]];
+    
+    UIView *labelSet2 = [self buildLabelSet];
+    [self.currentContainerView addSubview:labelSet2];
+    [NSLayoutConstraint activateConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[labelSet2]|"
+                                             options:0 metrics:0
+                                               views:NSDictionaryOfVariableBindings(labelSet2)]];
+    
+    UIView *labelSet3 = [self buildLabelSet];
+    [self.currentContainerView addSubview:labelSet3];
+    [NSLayoutConstraint activateConstraints:[NSLayoutConstraint
+                                             constraintsWithVisualFormat:@"H:|[labelSet3]|"
+                                             options:0 metrics:0
+                                             views:NSDictionaryOfVariableBindings(labelSet3)]];
+    
+    // Add and save the vertical constraints
+    self.verticalLabelConstraints = [NSLayoutConstraint
+                                     constraintsWithVisualFormat:@"V:|[labelSet1][labelSet2][labelSet3]-padding-|"
+                                     options:0 metrics:@{@"padding": @(kContinuousScrollViewLabelTopMargin)}
+                                     views:NSDictionaryOfVariableBindings(labelSet1, labelSet2, labelSet3)];
+    [NSLayoutConstraint activateConstraints:self.verticalLabelConstraints];
+}
+
+- (UIView *)buildLabelSet
+{
+    // Add the labels to a container view so we can move the containers around.
+    // This will give us the ability to make it appear like the user is scrolling
+    // since we will move the content view that may overlap the faux set of cells.
+    // While that is going we will move the opposite set to the direction the user
+    // is scrolling.
+    
+    // Start with the labelContainerView
+    UIView *labelContainerView = [[UIView alloc] init];
+    labelContainerView.translatesAutoresizingMaskIntoConstraints = NO;
+    labelContainerView.backgroundColor = [UIColor redColor];
+
     // Start fresh
     NSString *labelConstraintString = @"";
     NSMutableDictionary *labelConstraintDictionary = [[NSMutableDictionary alloc] init];
     
     // First check that our data source has some friends
-    
     if (_dataSource &&
         [_dataSource respondsToSelector:@selector(numberOfLabels)] &&
         [_dataSource respondsToSelector:@selector(labelAtIndex:)] &&
         [_dataSource numberOfLabels] > 0)
     {
         // Fill in the data we need
-        NSMutableArray *labelsLaidOut = [[NSMutableArray alloc] init];
-        
-        // Do this three times so we have cells on top and cells on bottom
         for (NSInteger index = 0; index < [_dataSource numberOfLabels]; ++index)
         {
             // Grab a label for the current index
@@ -104,30 +169,31 @@ NSInteger kContinuousScrollViewLabelSideMargin = 16;
             label.translatesAutoresizingMaskIntoConstraints = NO;
             
             // Officially add the label
-            [self.currentContainerView addSubview:label];
+            [labelContainerView addSubview:label];
             
             // Add the horizontal constraints
             [self addHorizontalConstraintsForLabel:label byName:viewName];
         }
-        self.labelsLaidout = labelsLaidOut;
         
         // Append the constraint to the bottom of the content view
         // Example: V:|-32-[label_0]-32-[label_1]-32-|
         if (labelConstraintString && ![labelConstraintString isEqualToString:@""])
         {
-            labelConstraintString = [NSString stringWithFormat:@"V:|%@-%zd-|", labelConstraintString,
-                                     kContinuousScrollViewLabelTopMargin];
+            labelConstraintString = [NSString stringWithFormat:@"V:|%@|", labelConstraintString];
             
             // Build the vertical constraints
-            NSArray *verticalConstraints =
+            self.verticalLabelConstraints =
             [NSLayoutConstraint constraintsWithVisualFormat:labelConstraintString
                                                     options:0 metrics:0
                                                       views:labelConstraintDictionary];
             
             // Add the vertical constraints
-            [self.currentContainerView addConstraints:verticalConstraints];
+            [NSLayoutConstraint activateConstraints:self.verticalLabelConstraints];
+            
         }
     }
+    
+    return labelContainerView;
 }
 
 - (void)addHorizontalConstraintsForLabel:(UILabel *)label byName:(NSString *)viewName
@@ -144,7 +210,7 @@ NSInteger kContinuousScrollViewLabelSideMargin = 16;
                                               views:@{viewName: label}];
     
     // Add the new found constraints
-    [self.currentContainerView addConstraints:horizontalConstraints];
+    [NSLayoutConstraint activateConstraints:horizontalConstraints];
 }
 
 - (void)calibratePosition
@@ -157,7 +223,6 @@ NSInteger kContinuousScrollViewLabelSideMargin = 16;
     
     if (distanceFromTheMiddle > (contentHeight / 4.0))
     {
-        NSLog(@"Move to the correct offset");
         self.contentOffset = CGPointMake(self.contentOffset.x, centerOffsetY);
     }
 }
@@ -166,7 +231,7 @@ NSInteger kContinuousScrollViewLabelSideMargin = 16;
 #pragma mark - UIScrollViewDelegate Methods
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
-//    [self calibratePosition];
+    [self calibratePosition];
 }
 
 @end
